5c5b12e5b7bea6b312ed6bea2558b84a35d1c5dc
zhenklchhh <zaleshchenok.e@mail.ru>
2025-09-21 12:00:26 +0300

    Initial commit

---
+++ src/main/java/com/innowise/innomicroservices/InnoMicroservicesApplication.java
@@ -0,0 +1,11 @@
+package com.innowise.innomicroservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InnoMicroservicesApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(InnoMicroservicesApplication.class, args);
+    }
+}
