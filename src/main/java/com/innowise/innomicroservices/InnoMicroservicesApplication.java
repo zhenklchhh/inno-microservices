5e613f72beb49b4e4adfc9132d87cd1caef4875b
zhenklchhh <zaleshchenok.e@mail.ru>
2025-10-03 11:21:57 +0300

    USR-5: implement Redis caching for User and Card services

--- src/main/java/com/innowise/innomicroservices/InnoMicroservicesApplication.java
+++ src/main/java/com/innowise/innomicroservices/InnoMicroservicesApplication.java
@@ -2,8 +2,10 @@ package com.innowise.innomicroservices;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
+@EnableCaching
 public class InnoMicroservicesApplication {
     public static void main(String[] args) {
         SpringApplication.run(InnoMicroservicesApplication.class, args);
