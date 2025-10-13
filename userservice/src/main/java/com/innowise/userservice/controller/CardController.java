02dc4cbc20ae3f702a465803c5f3123f01379e95
zhenklchhh <zaleshchenok.e@mail.ru>
2025-10-13 14:37:52 +0300

    add authorization for card endpoints

--- userservice/src/main/java/com/innowise/userservice/controller/CardController.java
+++ userservice/src/main/java/com/innowise/userservice/controller/CardController.java
@@ -6,6 +6,7 @@ import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -24,30 +25,35 @@ public class CardController {
     }
 
     @PostMapping
+    @PreAuthorize("hasRole('ADMIN')")
     public ResponseEntity<CardDto> createCard(@Valid @RequestBody CardDto createCardRequestDto) {
         CardDto cardResponseDto = cardService.createCard(createCardRequestDto);
         return ResponseEntity.status(HttpStatus.CREATED).body(cardResponseDto);
     }
 
     @GetMapping("/{id}")
+    @PreAuthorize("hasAnyRole('USER','ADMIN')")
     public ResponseEntity<CardDto> getCardById(@PathVariable Long id) {
         CardDto cardResponseDto = cardService.getCard(id);
         return ResponseEntity.ok(cardResponseDto);
     }
 
     @GetMapping
+    @PreAuthorize("hasRole('ADMIN')")
     public ResponseEntity<List<CardDto>> getAllCards() {
         List<CardDto> cardResponseDto = cardService.getAllCards();
         return ResponseEntity.ok(cardResponseDto);
     }
 
     @GetMapping(params = "ids")
+    @PreAuthorize("hasRole('ADMIN')")
     public ResponseEntity<List<CardDto>> getAllCards(@RequestParam List<Long> ids) {
         List<CardDto> cardResponseDtos = cardService.getCardsByIds(ids);
         return ResponseEntity.ok(cardResponseDtos);
     }
 
     @PutMapping("/{id}")
+    @PreAuthorize("hasRole('ADMIN')")
     public ResponseEntity<CardDto> updateCard(@PathVariable Long id,
                                               @Valid @RequestBody CardDto updateCardRequestDto) {
         CardDto cardResponseDto = cardService.updateCard(id, updateCardRequestDto);
@@ -55,6 +61,7 @@ public class CardController {
     }
 
     @DeleteMapping("/{id}")
+    @PreAuthorize("hasRole('ADMIN')")
     public ResponseEntity<Void> deleteCard(@PathVariable Long id) {
         cardService.deleteCard(id);
         return ResponseEntity.noContent().build();
