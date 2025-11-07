2bfb78b96728e247db9cf3f47df79e06d0371d31
zhenklchhh <zaleshchenok.e@mail.ru>
2025-11-07 15:58:44 +0300

    refactor: remove role checks, security configuration and jwt provider

--- userservice/src/main/java/com/innowise/userservice/controller/UserController.java
+++ userservice/src/main/java/com/innowise/userservice/controller/UserController.java
@@ -7,7 +7,6 @@ import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -26,49 +25,42 @@ public class UserController {
     }
 
     @PostMapping
-    @PreAuthorize("hasRole('ADMIN')")
     public ResponseEntity<UserDto> createUser(@Valid @RequestBody CreateUserRequestDto createUserRequestDTO) {
         UserDto userResponseDto = userService.createUser(createUserRequestDTO);
         return ResponseEntity.status(HttpStatus.CREATED).body(userResponseDto);
     }
 
     @GetMapping
-    @PreAuthorize("hasRole('ADMIN')")
     public ResponseEntity<List<UserDto>> getAllUsers() {
         List<UserDto> users = userService.getAllUsers();
         return ResponseEntity.status(HttpStatus.OK).body(users);
     }
 
     @GetMapping("/{id}")
-    @PreAuthorize("hasAnyRole('ADMIN', 'USER')")
     public ResponseEntity<UserDto> getUser(@PathVariable Long id) {
         UserDto userResponseDto = userService.getUserById(id);
         return ResponseEntity.ok(userResponseDto);
     }
 
     @GetMapping(params = "ids")
-    @PreAuthorize("hasRole('ADMIN')")
     public ResponseEntity<List<UserDto>> getUsersById(@RequestParam List<Long> ids) {
         List<UserDto> users = userService.getUsersByIds(ids);
         return ResponseEntity.ok(users);
     }
 
     @GetMapping(params = "email")
-    @PreAuthorize("hasRole('ADMIN')")
     public ResponseEntity<UserDto> getUserByEmail(@RequestParam String email) {
         UserDto userResponseDto = userService.getUserByEmail(email);
         return ResponseEntity.ok(userResponseDto);
     }
 
     @PutMapping("/{id}")
-    @PreAuthorize("hasRole('ADMIN')")
     public ResponseEntity<UserDto> updateUser(@PathVariable Long id,@Valid @RequestBody UserDto updateUserRequestDto) {
         UserDto userResponseDto = userService.updateUser(id, updateUserRequestDto);
         return ResponseEntity.ok(userResponseDto);
     }
 
     @DeleteMapping("/{id}")
-    @PreAuthorize("hasRole('ADMIN')")
     public ResponseEntity<Void> deleteUser(@PathVariable Long id) {
         userService.deleteUser(id);
         return ResponseEntity.noContent().build();
